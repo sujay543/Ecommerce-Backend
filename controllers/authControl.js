b1b777d8ab70fd40f7ff846212405ee41ecc0e9f
Sujay Sen <163733398+sujay543@users.noreply.github.com>
2026-02-24 03:54:19 +0530

    resume ecommerce project

--- controllers/authControl.js
+++ controllers/authControl.js
@@ -1,12 +1,27 @@
 const User = require('../models/userModel');
+const AppError = require('../utils/appError');
 
 exports.signUp = async (req,res,next) => {
-    const newUser = await User.create(req.body);
-    console.log(newUser);
+    if(!req.body.password || !req.body.username || !req.body.email)
+    {
+        return new AppError('we need password, username and email for signup',404);
+
+    }
+    const newUser = await User.create(
+        {
+     username : req.body.username,
+       email : req.body.email,
+       password : req.body.password,
+        confirmPassword  : req.body.confirmPassword
+        },{
+           new: true
+        }
+    );
+
     res.status(200).json(
         {
             status: 'success',
-            user: newUser
+            data: newUser
         }
     )
 }
--- models/userModel.js
+++ models/userModel.js
@@ -35,6 +35,11 @@ const userSchema = new mongoose.Schema(
             message: 'Password are not the same'
            }
               
+        },
+        role: {
+            type: String,
+            enum: ['admin','user'],
+            default: 'user'      
         }
     },
     {
