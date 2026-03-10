1689beeaf5e33033728d661b2808e332eed91869
Sujay Sen <163733398+sujay543@users.noreply.github.com>
2026-03-10 21:57:44 +0530

    final test before deployement

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,7 +5,7 @@ const productRouter = express.Router();
 
 productRouter.route('/').get(authControler.protect,productControler.getAllProducts).post(authControler.protect,authControler.restrictTo('admin'),productControler.addProducts);
 productRouter.route('/search').get(productControler.searchProduct);
-productRouter.route('/:id').get(productControler.getSpecificProducts).put(productControler.updateProducts).delete(authControler.protect,authControler.restrictTo('admin'),productControler.deleteProduct);
+productRouter.route('/:id').get(authControler.protect,productControler.getSpecificProducts).put(authControler.protect,productControler.updateProducts).delete(authControler.protect,authControler.restrictTo('admin'),productControler.deleteProduct);
 
 
 module.exports = productRouter;
