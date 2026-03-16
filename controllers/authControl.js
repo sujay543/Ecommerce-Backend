4eacd071fb5fc670c74d201d40a4c706a28fac69
Sujay Sen <163733398+sujay543@users.noreply.github.com>
2026-03-16 23:25:37 +0530

    fixing option expires

--- controllers/authControl.js
+++ controllers/authControl.js
@@ -10,12 +10,18 @@ const crypto = require('crypto');
 const createSendToken = (user,statusCode, res) => 
 {
     const token = jwt.sign({id: user._id},process.env.JWT_SECRET_KEY,{expiresIn: process.env.JWT_EXPIRY_DATE});
-    res.cookie('jwt',token,{
+    const cookieOptions = {
        expires: new Date(
             Date.now() + process.env.JWT_COOKIE_EXPIRY * 24 * 60 * 60 * 1000
         ),
         httpOnly: true,
-        });
+        }
+         if(process.env.NODE_ENV === 'production'){
+        cookieOptions.secure = true;
+    }
+    res.cookie('jwt',token,cookieOptions);
+
+   user.password = undefined;
     res.status(statusCode).json(
         {
             status: 'success',
